983dd04494259482b625b7b7bf6d16e4300e90d9
Aneta Borowski <borowski.aneta@gmail.com>
2022-11-13 22:34:16 +0100

    currency class renamed

--- js/script.js
+++ js/script.js
@@ -1,49 +1,49 @@
 let formElement = document.querySelector(".js-form");
 let toExchangeElement = document.querySelector(".js-toExchange");
 let toGetElement = document.querySelector(".js-toGet");
-let currency1Element = document.querySelector(".js-currency-1");
-let currency2Element = document.querySelector(".js-currency-2");
+let currencyFromElement = document.querySelector(".js-currencyFrom");
+let currencyToElement = document.querySelector(".js-currencyTo");
 let submitElement = document.querySelector(".js-submit");
 let resultElement = document.querySelector(".js-result");
 
 let PLN = 1;
-let EU = 4.67;
+let EUR = 4.67;
 let NOK = 0.46;
 
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  let currency1select;
-  let currency2select;
+  let currencyFromselect;
+  let currencyToselect;
   let toExchangeValue = +toExchangeElement.value;
   let result;
 
-  switch (currency1Element.value) {
+  switch (currencyFromElement.value) {
     case "PLN":
-      currency1select = PLN;
-      break;
+      currencyFromselect = PLN;
+      break;1
     case "€":
-      currency1select = EU;
+      currencyFromselect = EUR;
       break;
     case "NOK":
-      currency1select = NOK;
+      currencyFromselect = NOK;
       break;
   }
 
-  switch (currency2Element.value) {
+  switch (currencyToElement.value) {
     case "PLN":
-      currency2select = PLN;
+      currencyToselect = PLN;
       break;
     case "€":
-      currency2select = EU;
+      currencyToselect = EUR;
       break;
     case "NOK":
-      currency2select = NOK;
+      currencyToselect = NOK;
       break;
   }
 
-  result = (toExchangeValue * currency1select) / currency2select;
+  result = (toExchangeValue * currencyFromselect) / currencyToselect;
   toGetElement.value = result.toFixed(2);
 
-  resultElement.innerHTML = `Wymieniasz <strong>${toExchangeValue.toFixed(2)} ${currency1Element.value}</strong> i otrzymasz <strong>${result.toFixed(2)} ${currency2Element.value}</strong>`;
+  resultElement.innerHTML = `Wymieniasz <strong>${toExchangeValue.toFixed(2)} ${currencyFromElement.value}</strong> i otrzymasz <strong>${result.toFixed(2)} ${currencyToElement.value}</strong>`;
 });
